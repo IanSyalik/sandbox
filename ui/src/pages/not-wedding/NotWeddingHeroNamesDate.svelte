d75e625b59c0cdec7685abb88a63a3acfc6f0bf5
Ian Syalik <ian@codeborne.com>
2026-04-02 13:33:06 +0300

    refactor Not Wedding hero text for improved visibility and emphasis

--- ui/src/pages/not-wedding/NotWeddingHeroNamesDate.svelte
+++ ui/src/pages/not-wedding/NotWeddingHeroNamesDate.svelte
@@ -2,7 +2,7 @@
   @reference "./../../global.css";
 
   h1 {
-    @apply font-['Rouge_Script'] leading-[1] !text-[4rem] text-transparent px-1 py-0.5;
+    @apply font-['Rouge_Script'] leading-[1] !text-[5rem] text-transparent px-1 py-0.5;
     background-image: linear-gradient(
       180deg,
       color-mix(in srgb, var(--color-nw-700) 92%, transparent) 0%,
@@ -22,6 +22,6 @@
 <div class={` text-center ${containerClass}`.trim()}>
   <div class={`relative m-0 flex items-center justify-center text-nw-900 ${contentClass}`.trim()}>
     <h1>Jaan Jana</h1>
-    <h1 class="absolute left-1/2 top-1/2 ml-4 -translate-x-1/2 -translate-y-1/2 !text-[8rem] opacity-40">&</h1>
+    <h1 class="absolute left-1/2 top-1/2 ml-4 -translate-x-1/2 -translate-y-1/2 !text-[10rem] opacity-40">&</h1>
   </div>
 </div>
