9d36fd55815a21df445d627f73e1cf1ba6c0e8f0
Ian Syalik <ian@codeborne.com>
2026-04-03 17:07:49 +0300

    refactor NotWeddingHeroNamesDate component to adjust font size for improved visibility

--- ui/src/pages/not-wedding/NotWeddingHeroNamesDate.svelte
+++ ui/src/pages/not-wedding/NotWeddingHeroNamesDate.svelte
@@ -22,6 +22,6 @@
 <div class={`text-center ${containerClass}`.trim()}>
   <div class={`relative m-0 flex items-center justify-center text-nw-900 ${contentClass}`.trim()}>
     <h1 class="nw-hero-name">Jaan Jana</h1>
-    <h1 class="nw-hero-name nw-font-hero-ampersand absolute left-1/2 top-1/2 ml-3 -translate-x-1/2 -translate-y-1/2 opacity-40 lg:ml-4">&</h1>
+    <h1 class="nw-hero-name nw-font-hero-ampersand absolute left-1/2 top-1/2 ml-3 -translate-x-1/2 -translate-y-1/2 opacity-40 lg:ml-4 !text-[5rem] lg:!text-[8rem]">&</h1>
   </div>
 </div>
